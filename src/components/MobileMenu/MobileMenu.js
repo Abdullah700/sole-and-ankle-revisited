6aa6e26cac09e65e8e2c6d0bd443e39a661c2617
Abdullah700 <34545177+Abdullah700@users.noreply.github.com>
2022-01-23 14:33:18 +0300

    .

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,35 +3,98 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
-import { QUERIES } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
+import { startOfWeekYear } from 'date-fns';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+    <Content aria-label="Menu">
+      <CloseButton onClick={onDismiss}>
+      <Icon id={'close'}/>
+        <VisuallyHidden>
+          Dismiss menu
+        </VisuallyHidden>
+      </CloseButton>
+      <Filler/>
+           <Nav>
+        <NsvLink href="/sale">Sale</NsvLink>
+        <NsvLink href="/new">New&nbsp;Releases</NsvLink>
+        <NsvLink href="/men">Men</NsvLink>
+        <NsvLink href="/women">Women</NsvLink>
+        <NsvLink href="/kids">Kids</NsvLink>
+        <NsvLink href="/collections">Collections</NsvLink>
+      </Nav>
+      <Footer>
+        <FooterLink href="/terms">Terms and Conditions</FooterLink>
+        <FooterLink href="/privacy">Privacy Policy</FooterLink>
+        <FooterLink href="/contact">Contact Us</FooterLink>
+      </Footer>
+    </Content>
+    </Overlay>
   );
 };
+
+
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  inset: 0;
+  background-color: var(--color-backdrop);
+  display: flex;
+  justify-content: flex-end;
+`
+const Content = styled(DialogContent)`
+  background-color: #fff;
+  width: 300px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 32px;
+`
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`
+const NsvLink = styled.a`
+  color: var(--color-gray-900);
+  font-weight: ${WEIGHTS.medium};
+  text-decoration: none;
+  font-size: 1.125rem;
+  text-transform: uppercase;
+
+  &:first-of-type{
+    color: var(--color-secondary);
+  }
+
+`
+const Filler = styled.div`
+  flex:1;
+`
+
+const Footer = styled.footer`
+  flex:1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  gap: 12px;
+`
+const FooterLink = styled.a`
+  color: var(--color-gray-700);
+  text-decoration: none;
+  font-weight: ${WEIGHTS.normal};
+  font-size: 0.875rem;
+`
+const CloseButton = styled(UnstyledButton)`
+position: absolute;
+top: 10px;
+right: 0px;
+padding: 16px;
+`
 
 export default MobileMenu;
